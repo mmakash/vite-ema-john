e569dfb3fe996211b2f7f2ed698ed2e3dc9f505d
mmakash <musaddekurakash@gmail.com>
2023-04-29 02:29:27 +0600

    cart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Shop from './Components/Shop/Shop'
 
 function App() {
 
   return (
     <>
      <Header></Header>
+     <Shop></Shop>
     </>
   )
 }
