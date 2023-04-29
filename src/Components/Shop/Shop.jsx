f0a67353a675aa9b670bce072fe0b4f437145a71
mmakash <musaddekurakash@gmail.com>
2023-04-29 22:45:30 +0600

    order summary added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "./Shop.css";
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
@@ -29,7 +30,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h1>Order List</h1>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
